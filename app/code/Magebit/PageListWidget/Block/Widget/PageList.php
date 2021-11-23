cf6ecf8e54d1410c660b27cf81d8eedbb49ecf08
Rihards Edels <rihards.edels@gmail.com>
2021-11-23 16:36:47 +0200

    multi select added

---
+++ app/code/Magebit/PageListWidget/Block/Widget/PageList.php
@@ -0,0 +1,10 @@
+<?php
+namespace Magebit\PageListWidget\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class PageList extends Template implements BlockInterface
+{
+    protected $_template = "widget/page-list.phtml";
+}
---
+++ app/code/RihardsEdels/RihardsPage/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'RihardsEdels_RihardsPage',
+    __DIR__
+);
